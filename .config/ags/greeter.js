39001c8bbe72479d83258dfbf6416d3af84476b4
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-03-04 20:11:54 +0700

    feat!: use ags widgets

---
+++ .config/ags/greeter.js
@@ -0,0 +1,16 @@
+const main = "/tmp/ags/greeter.js"
+const entry = `${App.configDir}/greeter/greeter.ts`
+
+try {
+    await Utils.execAsync([
+        "bun", "build", entry,
+        "--outfile", main,
+        "--external", "resource://*",
+        "--external", "gi://*",
+        "--external", "file://*",
+    ])
+    await import(`file://${main}`)
+} catch (error) {
+    console.error(error)
+    App.quit()
+}
