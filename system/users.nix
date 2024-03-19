bde3b174262118478f396fad52eabc99f0465689
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-03-19 12:48:11 +0700

    feat: add user to docker group

--- system/users.nix
+++ system/users.nix
@@ -4,7 +4,7 @@
   users.users.hiepnh = {
     isNormalUser = true;
     description = "Nguyen Hong Hiep";
-    extraGroups = [ "networkmanager" "wheel" "video" "audio" "storage" "power" ];
+    extraGroups = [ "networkmanager" "wheel" "video" "audio" "storage" "power" "docker" ];
     shell = pkgs.fish;
     packages = with pkgs; [
       fish
