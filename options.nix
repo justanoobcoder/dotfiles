385523e08cc901fb7af6ffea3700ca7fc287f9ef
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-03-19 21:30:51 +0700

    feat: remove some unuse variables

--- options.nix
+++ options.nix
@@ -9,24 +9,15 @@ in {
   hostname = "nixos-pc";
   gitUsername = "Nguyen Hong Hiep";
   gitEmail = "syaorancode@gmail.com";
-  browser = "floorp";
   flakeDir = "${flakeDir}";
-  terminal = "alacritty";
 
   # System settings
   locale = "en_US.UTF-8";
   keyboardLayout = "us";
   timezone = "Asia/Ho_Chi_Minh";
   shell = "fish";
-  theKernel = "lastest";
 
   # Enable Support For
   # Logitech Devices
   logitech = true;
-
-  # Enable Terminals
-  alacritty = true;
-
-  # Enable SyncThing
-  syncthing = false;
 }
