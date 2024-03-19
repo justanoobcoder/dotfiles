8ebc0f96c0ca6ad51c02b98e355a93f60d8069a3
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-03-19 16:50:47 +0700

    style: format

--- home/config/rofi/launcher/launcher.sh
+++ home/config/rofi/launcher/launcher.sh
@@ -3,8 +3,8 @@
 config_file="${HOME}/.config/rofi/launcher/config.rasi"
 
 rofi -no-lazy-grab \
-    -show drun \
-    -modi drun \
-    -no-config \
-    -theme ${config_file} \
-    2> /dev/null
+	-show drun \
+	-modi drun \
+	-no-config \
+	-theme ${config_file} \
+	2>/dev/null
