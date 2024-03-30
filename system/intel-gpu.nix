b911d295b65cde01b154734ab47aca668158a084
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-03-30 20:14:30 +0700

    feat: rollback

--- system/intel-gpu.nix
+++ system/intel-gpu.nix
@@ -6,16 +6,18 @@ in
 lib.mkIf ("${gpu}" == "intel")
 {
   nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
   };
 
   # OpenGL
   hardware.opengl = {
+    enable = true;
     extraPackages = with pkgs; [
       intel-media-driver
-      vaapiIntel
       vaapiVdpau
       libvdpau-va-gl
     ];
   };
+
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
 }
