2844bf87be433c749e1068cb705bde8d817790a4
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-05-05 18:38:43 +0700

    feat: change default terminal to foot

--- home/nix/cli/terminals.nix
+++ home/nix/cli/terminals.nix
@@ -1,6 +1,39 @@
 let
   inherit (import ../../../options.nix) terminal;
 
+  footConfig = {
+    foot = {
+      enable = true;
+      server.enable = true;
+      settings = {
+        main = {
+            font = "JetBrainsMono Nerd Font:size=9.5";
+        };
+        colors = {
+          alpha = terminal.opacity;
+          background = terminal.colorScheme.primary.background;
+          foreground = terminal.colorScheme.primary.foreground;
+          regular0 = terminal.colorScheme.normal.black;
+          regular1 = terminal.colorScheme.normal.red;
+          regular2 = terminal.colorScheme.normal.green;
+          regular3 = terminal.colorScheme.normal.yellow;
+          regular4 = terminal.colorScheme.normal.blue;
+          regular5 = terminal.colorScheme.normal.magenta;
+          regular6 = terminal.colorScheme.normal.cyan;
+          regular7 = terminal.colorScheme.normal.white;
+          bright0 = terminal.colorScheme.bright.black;
+          bright1 = terminal.colorScheme.bright.red;
+          bright2 = terminal.colorScheme.bright.green;
+          bright3 = terminal.colorScheme.bright.yellow;
+          bright4 = terminal.colorScheme.bright.blue;
+          bright5 = terminal.colorScheme.bright.magenta;
+          bright6 = terminal.colorScheme.bright.cyan;
+          bright7 = terminal.colorScheme.bright.white;
+        };
+      };
+    };
+  };
+
   kittyConfig = {
     kitty = {
       enable = true;
@@ -18,24 +51,24 @@ let
 
         background_opacity = "${toString terminal.opacity}";
 
-        foreground = terminal.colorScheme.primary.foreground;
-        background = terminal.colorScheme.primary.background;
-        color0 = terminal.colorScheme.normal.black;
-        color1 = terminal.colorScheme.normal.red;
-        color2 = terminal.colorScheme.normal.green;
-        color3 = terminal.colorScheme.normal.yellow;
-        color4 = terminal.colorScheme.normal.blue;
-        color5 = terminal.colorScheme.normal.magenta;
-        color6 = terminal.colorScheme.normal.cyan;
-        color7 = terminal.colorScheme.normal.white;
-        color8 = terminal.colorScheme.bright.black;
-        color9 = terminal.colorScheme.bright.red;
-        color10 = terminal.colorScheme.bright.green;
-        color11 = terminal.colorScheme.bright.yellow;
-        color12 = terminal.colorScheme.bright.blue;
-        color13 = terminal.colorScheme.bright.magenta;
-        color14 = terminal.colorScheme.bright.cyan;
-        color15 = terminal.colorScheme.bright.white;
+        foreground = "#${terminal.colorScheme.primary.foreground}";
+        background = "#${terminal.colorScheme.primary.background}";
+        color0 = "#${terminal.colorScheme.normal.black}";
+        color1 = "#${terminal.colorScheme.normal.red}";
+        color2 = "#${terminal.colorScheme.normal.green}";
+        color3 = "#${terminal.colorScheme.normal.yellow}";
+        color4 = "#${terminal.colorScheme.normal.blue}";
+        color5 = "#${terminal.colorScheme.normal.magenta}";
+        color6 = "#${terminal.colorScheme.normal.cyan}";
+        color7 = "#${terminal.colorScheme.normal.white}";
+        color8 = "#${terminal.colorScheme.bright.black}";
+        color9 = "#${terminal.colorScheme.bright.red}";
+        color10 = "#${terminal.colorScheme.bright.green}";
+        color11 = "#${terminal.colorScheme.bright.yellow}";
+        color12 = "#${terminal.colorScheme.bright.blue}";
+        color13 = "#${terminal.colorScheme.bright.magenta}";
+        color14 = "#${terminal.colorScheme.bright.cyan}";
+        color15 = "#${terminal.colorScheme.bright.white}";
       };
     };
   };
@@ -47,9 +80,30 @@ let
       settings = {
         colors = {
           draw_bold_text_with_bright_colors = true;
-          bright = terminal.colorScheme.bright;
-          normal = terminal.colorScheme.normal;
-          primary = terminal.colorScheme.primary;
+          bright = {
+            black = "#${terminal.colorScheme.bright.black}";
+            blue = "#${terminal.colorScheme.bright.blue}";
+            cyan = "#${terminal.colorScheme.bright.cyan}";
+            green = "#${terminal.colorScheme.bright.green}";
+            magenta = "#${terminal.colorScheme.bright.magenta}";
+            red = "#${terminal.colorScheme.bright.red}";
+            white = "#${terminal.colorScheme.bright.white}";
+            yellow = "#${terminal.colorScheme.bright.yellow}";
+          };
+          normal = {
+            black = "#${terminal.colorScheme.normal.black}";
+            blue = "#${terminal.colorScheme.normal.blue}";
+            cyan = "#${terminal.colorScheme.normal.cyan}";
+            green = "#${terminal.colorScheme.normal.green}";
+            magenta = "#${terminal.colorScheme.normal.magenta}";
+            red = "#${terminal.colorScheme.normal.red}";
+            white = "#${terminal.colorScheme.normal.white}";
+            yellow = "#${terminal.colorScheme.normal.yellow}";
+          };
+          primary = {
+            background = "#${terminal.colorScheme.primary.background}";
+            foreground = "#${terminal.colorScheme.primary.foreground}";
+          };
         };
 
         window = {
@@ -110,5 +164,11 @@ let
   };
 in
 {
-  programs = if terminal.name == "kitty" then kittyConfig else alacrittyConfig;
+  programs =
+    if terminal.name == "kitty" then
+      kittyConfig
+    else if terminal.name == "foot" then
+      footConfig
+    else
+      alacrittyConfig;
 }
