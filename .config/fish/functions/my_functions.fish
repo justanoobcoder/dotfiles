7099f63109826af05b1b5c6ee9d029658a302330
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-12-16 09:32:23 +0700

    feat: create function for yazi

--- .config/fish/functions/my_functions.fish
+++ .config/fish/functions/my_functions.fish
@@ -70,3 +70,12 @@ function frparu
             '$AUR_HELPER -Si {1}' | xargs -ro $AUR_HELPER -Rns
     end
 end
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
