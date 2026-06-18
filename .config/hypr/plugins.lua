baacac4fb368716145e5abd4f6d3e7a25be783ee
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-06-18 20:31:52 +0700

    hypr: change scale value of scrolloverview to 0.3

--- .config/hypr/plugins.lua
+++ .config/hypr/plugins.lua
@@ -1,7 +1,7 @@
 if hl.plugin and hl.plugin.scrolloverview then
 	hl.plugin.scrolloverview.configure({
 		gesture_distance = 300, -- how far is the "max" for the gesture
-		scale = 0.4,
+		scale = 0.3,
 		workspace_gap = 10,
 		wallpaper = 0, -- 0: global only, 1: per-workspace only, 2: both
 		blur = false,
